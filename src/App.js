c944ef81278fa9a4e4b393dae4ed9ce0d99f4bf8
Arison13 <arisonarias@gmail.com>
2021-10-28 11:40:56 -0400

    got basic dom built, still need to find a way to set followers with componentdidupdate and display, and then modularize the code to make it more dry

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    followers:[],
+    info:{}
+  }
+
+   componentDidMount() {
+    axios.get(' https://api.github.com/users/Arison13')
+    .then (res => {
+      this.setState({info:res.data})
+      console.log(res.data)
+    }).catch(err => {
+      console.error(err)
+    })
+  }
+
+  // componentDidUpdate(){
+  //   axios.get("https://api.github.com/users/Arison13/followers")
+  //   .then(res=> {
+  //     console.log("component update data:", res.data)
+  //     // this.setState({
+  //     //   followers: res.data
+  //     // })
+  //   }).catch(err => console.err(err))
+  // }
+  
   render() {
     return(<div>
-      Github Card
+      <h1>Github Card</h1>
+      <div className="searchBar">
+        <input type="search" placeholder="Github Handle" /> 
+        <button>Search</button>
+        
+        <div className="user-info">
+          <h4> {this.state.info.login}</h4>
+          <img width="100"src={this.state.info.avatar_url} alt={this.state.info.avatar_url}/>
+          <p>Total Repos: {this.state.info.public_repos}</p>
+          <p>Total Followers: {this.state.info.followers}</p>
+        </div>
+
+        <div className="Followers"> 
+
+
+        </div>
+      </div>
     </div>);
   }
 }
